12fe0bb5568eccbc3af92fff0859119dc8da4354
Itziar Morales Rodríguez <itziar.devops@itziar97.com>
2023-11-20 18:09:16 +0100

    Hardware

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -1,8 +1,8 @@
 # Edit this configuration file to define what should be installed on
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { inputs, config, lib, osConfig, pkgs, ... }:
+
 
 {
   imports =
@@ -164,7 +164,7 @@
   # $ nix search wget
   environment.systemPackages = with pkgs; [
     firefox
-    vim 
+    vim
     neovim
     home-manager
     wget
